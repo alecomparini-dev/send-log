8f2d0f1fc24888e23772b6e5c5ea3365f69345f0
comparini <alecomparini.dev@gmail.com>
2022-01-26 15:41:45 -0300

    included log file, model file and updated function token_is_valid

--- oauth.py
+++ oauth.py
@@ -1,9 +1,10 @@
+import jwt
 import requests
+from datetime import datetime
 from dotenv import dotenv_values
 config = dotenv_values(".env")
 
 class OAuth:
-
     _client_id = config['CLIENT_ID']
     _client_secret = config['CLIENT_SECRET']
     _url = config['URL_TOKEN']
@@ -11,7 +12,6 @@ class OAuth:
     def __init__(self):
         self._token_expires_in = 0
         self._token = ''
-
 
     def get_new_token(self):
         token_req_payload = {'grant_type': 'client_credentials'}
@@ -23,19 +23,13 @@ class OAuth:
 
         token_resp = resp.json()
         self._token = token_resp['access_token']
-        self._token_expires_in = token_resp['expires_in']
-
+        self._token_expires_in = jwt.decode(self._token, options={"verify_signature": False})['exp']
 
     def token_is_valid(self):
-        return True
+        if (self._token_expires_in - datetime.now().timestamp()) <= 0:
+            self.get_new_token()
 
     @property
     def token(self):
+        self.token_is_valid()
         return self._token
-
-    @token.setter
-    def token(self, token):
-        self._token = token
-
-
-
